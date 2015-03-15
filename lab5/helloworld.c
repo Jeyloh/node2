c3e138446a034bd6504f18b727314d9ca157522b
Jeyloh <jlh.94@hotmail.com>
2015-03-15 21:45:46 +0100

    lab5

---
+++ lab5/helloworld.c
@@ -0,0 +1,10 @@
+// hello.c
+#include <stdio.h>
+ 
+int main() {
+    printf("Hello, world!\n");
+    printf("Checking out C!\n");
+    return 0;
+}
+
+
---
+++ lab5/lab5.py
@@ -0,0 +1,23 @@
+#   A script that simulates a poker game. Input how many players and how many cards in each hand, 
+#   then return the best rank and declare a winner.
+#
+#
+#   Author:     Jorgen Lybeck Hansen, Elaine Sajets, Daniel Eide, Bastian Strang, Jonas Dam, Christian Thorne
+#   Group:      Node
+#   Version:    02.23.2015
+#   Notes:
+#               I followed the UDACITY: Design of Computer Programs course while trying to write
+#               my own functions. 
+#
+#   Bugs: 
+#		         None
+#
+""" LAB 5
+Oppgaven bør hjelpe dere til å forstå måten et miljø for utførelse av
+dataprogrammer (les operativsystem) er designet på. Dere er allered litt kjent med
+kommandovinduet (shell, terminalvindu), som er et “vindu” inn i operativsystemet.
+Gjennom dette “vinduet” og ved hjelp av andre programmer (som python, for
+eksempel), kan man følge “liv” til et program fra den blir skrevet av dere, til den blir
+representert som en prosess i et datasystem.
+"""
+
