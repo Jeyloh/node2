373e467f4b0bce40877f36c42901a7a633d57d50
Jeyloh <jlh.94@hotmail.com>
2015-05-15 21:30:51 +0200

    lab3 100%

--- lab3/server.py
+++ lab3/server.py
@@ -4,28 +4,6 @@
 #   Author:     Jorgen Lybeck Hansen, Elaine Sajets, Daniel Eide, Bastian Strang, Jonas Dam, Christian Thorne
 #   Group:      Node
 
-"""Oppgaver som mangler: 
-
-4. Sjekk om applikasjonen fungerer for tegn utover ASCII tegnsettet og forklar
-hva som skjer. Gjør om applikasjonen slik at den kan overføre hvilke som
-helst tegn fra UNICODE karaktersettet. Finnes det en sammenheng mellom
-den binære representasjonen for små og store bokstaver også i dette tilfelle?
-Forklar.
-
-5. Implementer din egen metode i for overføring fra små til store bokstaver
-basert på manipulasjon av den binære representasjonen til tegn. Bruk
-rutiner fra LAB1 (operasjoner på bit). Implementer tester for alle rutiner i en
-egen funksjon test() ved bruk av assert setninger.
-
-6. Bruk funksjoner fra deres “romerske” modulen for å implementere denne
-fuksjonaliteten i en klient-tjener applikasjon. For å få det til, må dere endre
-grensesnitt for INNDATA (i klienten). Her kan dere bruke den innebygde
-funksjonen i Python raw_input(). Pass på at dere tester for alle eventuelle
-feil, som en kommandolinje bruker potensielt kan gjøre.
-
-7. Burde dere bruke TCP protokollen for dere applikasjon i dette tilfelle? Forklar
-hvorfor, eventuelt hvorfor ikke
-"""
 from socket import *
 
 serverPort = 12000
@@ -47,55 +25,61 @@ while 1:
 
 	#modifiedMessage = message.decode('utf-8').upper().encode('utf-8')
 
+
 	def char_to_bit(character):
-		
-		output = []
+		"Brukes for å gjøre message om til bits."
+		bit_list = []
 
+		# Hvert element i character refereres til som c
 		for c in character:
+			# ord() gir oss en integer verdi av hver c
 			n = ord(c)
+			# Formaterer n som er integer verdier av hver c
 			b = "{0:0b}".format(n)
-			output.append(b)
+			# Legger til det ferdige resultatet i listen.
+			bit_list.append(b)
 
-		print output
-		for b in output[:-1]:
+		print bit_list
+
+		# Denne loopen sjekker hver byte i bit_list og ser om de er større enn 7.
+		# Visst de er større enn 7 vet vi at det er unicode, f. eks 'æøå'
+		for b in bit_list[:-1]:
 			if len(b) > 7:
 				if b[0] == "1" and b[1] == "1":
-					temp = b + output[output.index(b)+1]
+					temp = b + bit_list[bit_list.index(b)+1]
 					print temp
-					output.insert(output.index(b), temp)
-					output.remove(output[output.index(b) + 1])
-					output.remove(b)
+					bit_list.insert(bit_list.index(b), temp)
+					bit_list.remove(bit_list[bit_list.index(b) + 1])
+					bit_list.remove(b)
 
-		print output
-		return output
+		print bit_list
+		return bit_list
 
 	def to_upper(message):
-		
-		output = []
+		dec_list = []
 
 		for c in message:
 			c = str(c)
 
+			# Vi formaterer integeren vår med '2' som er binary formatering
 			to_dec = int(c, 2) #50085
+			# Trekker 32 fra integer for å få uppercase int til lowercasen vår
 			upper = to_dec - 32
+			# Legger alt til i listen og fjerner hex (u\)
+			dec_list.append(("%x" % upper).decode('hex')) # 
+		dec_list = ''.join(dec_list)
+		return dec_list
 
-			output.append(("%x" % upper).decode('hex'))
-		output = ''.join(output)
-		return output
-
+	# Bruker begge funksjonene våre på message (sendt fra klient)
 	modifiedMessage = to_upper(char_to_bit(message))
-
-
-
 	print modifiedMessage
-
-	
 
 	# Socket function sendto() sends the modifiedMessage to the clientAddress
 	serverSocket.sendto(modifiedMessage, clientAddress)
 
 
-def test():
-	pass
+
+
+
 
 
